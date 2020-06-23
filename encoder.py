a19f85a9495230161a2f6d9f5e868810b5e0be32
Harshveer <harshveer321.code@gmail.com>
2020-06-23 20:04:03 +0530

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -3,72 +3,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import BertPreTrainedModel, BertModel, DistilBertModel
-from model import dot_attention
+from attn_mech import dot_attention
 
-
-class BertDssmModel(BertPreTrainedModel):
-  def __init__(self, config, *inputs, **kwargs):
-    super().__init__(config, *inputs, **kwargs)
-    self.bert = kwargs['bert']
-    try:
-      self.dropout = nn.Dropout(config.hidden_dropout_prob)
-      self.context_fc = nn.Linear(config.hidden_size, 64)
-      self.response_fc = nn.Linear(config.hidden_size, 64)
-    except:
-      self.dropout = nn.Dropout(config.dropout)
-      self.context_fc = nn.Linear(config.dim, 64)
-      self.response_fc = nn.Linear(config.dim, 64)
-
-  def forward(self, context_input_ids, context_segment_ids, context_input_masks,
-              responses_input_ids, responses_segment_ids, responses_input_masks, labels=None):
-    ## only select the first response (whose lbl==1)
-    if labels is not None:
-      responses_input_ids = responses_input_ids[:, 0, :].unsqueeze(1)
-      responses_segment_ids = responses_segment_ids[:, 0, :].unsqueeze(1)
-      responses_input_masks = responses_input_masks[:, 0, :].unsqueeze(1)
-
-    if isinstance(self.bert, DistilBertModel):
-      context_vec = self.bert(context_input_ids, context_input_masks)[-1]  # [bs,dim]
-      context_vec = context_vec[:, 0]
-    else:
-      context_vec = self.bert(context_input_ids, context_input_masks, context_segment_ids)[-1]  # [bs,dim]
-
-    batch_size, res_cnt, seq_length = responses_input_ids.shape
-    responses_input_ids = responses_input_ids.view(-1, seq_length)
-    responses_input_masks = responses_input_masks.view(-1, seq_length)
-    responses_segment_ids = responses_segment_ids.view(-1, seq_length)
-
-    if isinstance(self.bert, DistilBertModel):
-      responses_vec = self.bert(responses_input_ids, responses_input_masks)[-1]  # [bs,dim]
-      responses_vec = responses_vec[:, 0]
-    else:
-      responses_vec = self.bert(responses_input_ids, responses_input_masks, responses_segment_ids)[
-        -1]  # [bs,dim]
-    responses_vec = responses_vec.view(batch_size, res_cnt, -1)
-
-    context_vec = self.context_fc(self.dropout(context_vec))
-    context_vec = F.normalize(context_vec, 2, -1)
-
-    responses_vec = self.response_fc(self.dropout(responses_vec))
-    responses_vec = F.normalize(responses_vec, 2, -1)
-
-    if labels is not None:
-      responses_vec = responses_vec.squeeze(1)
-      dot_product = torch.matmul(context_vec, responses_vec.t())  # [bs, bs]
-      mask = torch.eye(context_input_ids.size(0)).to(context_input_ids.device)
-      loss = F.log_softmax(dot_product * 5, dim=-1) * mask
-      loss = (-loss.sum(dim=1)).mean()
-
-      return loss
-    else:
-      context_vec = context_vec.unsqueeze(1)
-      dot_product = torch.matmul(context_vec, responses_vec.permute(0, 2, 1))  # take this as logits
-      dot_product.squeeze_(1)
-      cos_similarity = (dot_product + 1) / 2
-      return cos_similarity
-
-
-class BertPolyDssmModel(BertPreTrainedModel):
+class BertPolyModel(BertPreTrainedModel):
   def __init__(self, config, *inputs, **kwargs):
     super().__init__(config, *inputs, **kwargs)
     self.bert = kwargs['bert']
